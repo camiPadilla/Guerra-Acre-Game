ea38c928e9d3723ffabb63249a43afa8dacbc06d
cami <cami_padilla@outlook.com>
2025-09-17 15:59:48 -0400

    inicio para codigo del loadscreen

--- Assets/Scripts/LoadingScene/LoadScreenAnimation.cs
+++ Assets/Scripts/LoadingScene/LoadScreenAnimation.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 public class LoadScreenAnimation : MonoBehaviour
 {
+    public Image tigrillo;
+    public Image ave;
+    public Image soldado1;
+    public Image soldado2;
+    public Image coca;
     private void OnEnable()
     {
         //Animacion para el load Screen
+    }
+    private void Tigrillo()
+    {
+
+    }
+    private void Soldado1() {
+        
+    }
+    private void Soldado2()
+    {
+
     }
 }
