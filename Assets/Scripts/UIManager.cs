d2357e6fdca2d270aae365e9dc2c2de67df422d8
ElTaqui <luis4lexander@gmail.com>
2025-09-19 20:57:31 -0400

    Guardado y cerrado de escena anterior

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] GameObject menu;
+    [SerializeField] GameObject coleccionables;
+    [SerializeField] GameObject partida;
+    [SerializeField] GameObject salir;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +19,40 @@ public class UIManager : MonoBehaviour
     {
         
     }
-   
+    public void IrMenu()
+    {
+        menu.SetActive(true);
+        coleccionables.SetActive(false);
+        partida.SetActive(false);
+        salir.SetActive(false);
+    }
+    public void IrColeccionables()
+    {
+        menu.SetActive(false);
+        coleccionables.SetActive(true);
+        partida.SetActive(false);
+        salir.SetActive(false);
+    }
+    public void IrMenuPartida()
+    {
+        menu.SetActive(false);
+        coleccionables.SetActive(false);
+        partida.SetActive(true);
+        salir.SetActive(false);
+    }
+    public void IrSalir()
+    {
+        menu.SetActive(false);
+        coleccionables.SetActive(false);
+        partida.SetActive(false);
+        salir.SetActive(true);
+    }
+    public void Salir()
+    {
+        Application.Quit();
+    }
+    public void Comezar()
+    {
+         
+    }
 }
