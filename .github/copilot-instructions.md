e5de6d27de74183f160342732d31abeaf27001fd
cami <cami_padilla@outlook.com>
2025-10-08 16:04:15 -0400

    aumento de ui

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -2,13 +2,13 @@
 - Project name: Guerra-del-Acre-Game
 - Unity version: Unity 2022.3.62f1
 - Active scene:
-  - Name: EscenaUno
+  - Name: MainMenu
   - Tags:
     - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, hitbox, Destruible, Escenario, movible, NPC, Enemigo, LoaderScene, caja
   - Layers:
     - Default, TransparentFX, Ignore Raycast, Piso, Water, UI, Player
 - Active game object:
-  - Name: cabeza
+  - Name: Menu Inicio
   - Tag: Untagged
-  - Layer: Default
+  - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,15 +6,8 @@ using UnityEngine.Audio;
 
 public class UIManager : MonoBehaviour
 {
-
-    [SerializeField] GameObject menu;
-    [SerializeField] GameObject coleccionables;
-    [SerializeField] GameObject partida;
-    [SerializeField] GameObject salir;
-    [SerializeField] GameObject opciones;
-    [SerializeField] GameObject audioo;
-    [SerializeField] GameObject controles;
-    [SerializeField] GameObject video;
+    [SerializeField] List<GameObject> pantallasMenu;
+    
 
     [SerializeField] string escenaPartida;
 
@@ -31,102 +24,102 @@ public class UIManager : MonoBehaviour
     }
     public void IrMenu()
     {
-        menu.SetActive(true);
-        coleccionables.SetActive(false);
-        partida.SetActive(false);
-        salir.SetActive(false);
-        opciones.SetActive(false);
-        audioo.SetActive(false);
-        controles.SetActive(false);
-        video.SetActive(false);
+        pantallasMenu[0].SetActive(true);
+        pantallasMenu[1].SetActive(false);
+        pantallasMenu[2].SetActive(false);
+        pantallasMenu[3].SetActive(false);
+        pantallasMenu[4].SetActive(false);
+        pantallasMenu[5].SetActive(false);
+       pantallasMenu[6].SetActive(false);
+        pantallasMenu[7].SetActive(false);
     }
     public void IrColeccionables()
     {
-        menu.SetActive(false);
-        coleccionables.SetActive(true);
-        partida.SetActive(false);
-        salir.SetActive(false);
-        opciones.SetActive(false);
-        audioo.SetActive(false);
-        controles.SetActive(false);
-        video.SetActive(false);
+        pantallasMenu[0].SetActive(false);
+        pantallasMenu[1].SetActive(true);
+        pantallasMenu[2].SetActive(false);
+        pantallasMenu[3].SetActive(false);
+        pantallasMenu[4].SetActive(false);
+       pantallasMenu[5].SetActive(false);
+        pantallasMenu[6].SetActive(false);
+        pantallasMenu[7].SetActive(false);
     }
     public void IrMenuPartida()
     {
-        menu.SetActive(false);
-        coleccionables.SetActive(false);
-        partida.SetActive(true);
-        salir.SetActive(false);
-        opciones.SetActive(false);
-        audioo.SetActive(false);
-        controles.SetActive(false);
-        video.SetActive(false);
+         pantallasMenu[0].SetActive(false);
+        pantallasMenu[1].SetActive(false);
+        pantallasMenu[2].SetActive(true);
+        pantallasMenu[3].SetActive(false);
+        pantallasMenu[4].SetActive(false);
+        pantallasMenu[5].SetActive(false);
+        pantallasMenu[6].SetActive(false);
+        pantallasMenu[7].SetActive(false);
     }
     public void IrSalir()
     {
-        menu.SetActive(false);
-        coleccionables.SetActive(false);
-        partida.SetActive(false);
-        salir.SetActive(true);
-        opciones.SetActive(false);
-        audioo.SetActive(false);
-        controles.SetActive(false);
-        video.SetActive(false);
+         pantallasMenu[0].SetActive(false);
+        pantallasMenu[1].SetActive(false);
+        pantallasMenu[2].SetActive(false);
+        pantallasMenu[3].SetActive(true);
+        pantallasMenu[4].SetActive(false);
+        pantallasMenu[5].SetActive(false);
+       pantallasMenu[6].SetActive(false);
+        pantallasMenu[7].SetActive(false);
     }
     public void IrOpciones()
     {
-        menu.SetActive(false);
-        coleccionables.SetActive(false);
-        partida.SetActive(false);
-        salir.SetActive(false);
-        opciones.SetActive(true);
-        audioo.SetActive(false);
-        controles.SetActive(false);
-        video.SetActive(false);
+        pantallasMenu[0].SetActive(false);
+        pantallasMenu[1].SetActive(false);
+        pantallasMenu[2].SetActive(false);
+        pantallasMenu[3].SetActive(false);
+        pantallasMenu[4].SetActive(true);
+        pantallasMenu[5].SetActive(false);
+        pantallasMenu[6].SetActive(false);
+        pantallasMenu[7].SetActive(false);
     }
     public void IrAudio()
     {
-        menu.SetActive(false);
-        coleccionables.SetActive(false);
-        partida.SetActive(false);
-        salir.SetActive(false);
-        opciones.SetActive(false);
-        audioo.SetActive(true);
-        controles.SetActive(false);
-        video.SetActive(false);
+        pantallasMenu[0].SetActive(false);
+        pantallasMenu[1].SetActive(false);
+        pantallasMenu[2].SetActive(false);
+        pantallasMenu[3].SetActive(false);
+        pantallasMenu[4].SetActive(false);
+        pantallasMenu[5].SetActive(true);
+        pantallasMenu[6].SetActive(false);
+        pantallasMenu[7].SetActive(false);
     }
     public void IrControles()
     {
-        menu.SetActive(false);
-        coleccionables.SetActive(false);
-        partida.SetActive(false);
-        salir.SetActive(false);
-        opciones.SetActive(false);
-        audioo.SetActive(false);
-        controles.SetActive(true);
-        video.SetActive(false);
+         pantallasMenu[0].SetActive(false);
+        pantallasMenu[1].SetActive(false);
+        pantallasMenu[2].SetActive(false);
+        pantallasMenu[3].SetActive(false);
+        pantallasMenu[4].SetActive(false);
+        pantallasMenu[5].SetActive(false);
+        pantallasMenu[6].SetActive(true);
+        pantallasMenu[7].SetActive(false);
     }
     public void IrVideo()
     {
-        menu.SetActive(false);
-        coleccionables.SetActive(false);
-        partida.SetActive(false);
-        salir.SetActive(false);
-        opciones.SetActive(false);
-        audioo.SetActive(false);
-        controles.SetActive(false);
-        video.SetActive(true);
+         pantallasMenu[0].SetActive(false);
+        pantallasMenu[1].SetActive(false);
+        pantallasMenu[2].SetActive(false);
+        pantallasMenu[3].SetActive(false);
+        pantallasMenu[4].SetActive(false);
+        pantallasMenu[5].SetActive(false);
+        pantallasMenu[6].SetActive(false);
+        pantallasMenu[7].SetActive(true);
     }
     public void Desactivado()
     {
-        menu.SetActive(false);
-         coleccionables.SetActive(false);
-        partida.SetActive(false);
-        salir.SetActive(false);
-        opciones.SetActive(false);
-        audioo.SetActive(false);
-        controles.SetActive(false);
-        video.SetActive(false);
+         pantallasMenu[0].SetActive(false);
+         pantallasMenu[1].SetActive(false);
+        pantallasMenu[2].SetActive(false);
+        pantallasMenu[3].SetActive(false);
+        pantallasMenu[4].SetActive(false);
+       pantallasMenu[5].SetActive(false);
+        pantallasMenu[6].SetActive(false);
+        pantallasMenu[7].SetActive(false);
     }
     public void Salir()
     {
