8921b0bf6a04182e51cce2a4aeb6303a26e37c10
el-Marce <gallardocoriamarcelodaniel@gmail.com>
2025-10-09 19:57:39 -0400

    Sonidos de menu + control de volumen TODO FUNCIONA
    Actualézame o moriré D:

--- Assets/Scripts/Sonidos/UISoundManager.cs
+++ Assets/Scripts/Sonidos/UISoundManager.cs
@@ -1,5 +1,8 @@
-using UnityEngine;
+﻿using UnityEngine;
 using FMODUnity;
+using UnityEngine.UI;
+using FMOD.Studio;
+using static UnityEngine.Rendering.DebugUI;
 
 public class UISoundManager : MonoBehaviour
 {
@@ -9,7 +12,15 @@ public class UISoundManager : MonoBehaviour
     [SerializeField] EventReference hoverJochi;
     [SerializeField] EventReference music;
 
-
+    [SerializeField] private Scrollbar masterVolume;
+    private void Start()
+    {
+    
+    }
+    void Update()
+    {        
+        ActualizarMasterVolume();   
+    }
 
     public void PlayHover()
     {
@@ -33,5 +44,16 @@ public class UISoundManager : MonoBehaviour
     {
         if (!hoverJochi.IsNull)
             RuntimeManager.PlayOneShot(hoverJochi);
+    }
+
+    public void ActualizarMasterVolume() 
+    {
+        
+        float volume = masterVolume.value;
+        //Debug.Log("Valor actual del Scrollbar: " + volume);
+
+        RuntimeManager.StudioSystem.setParameterByName("MasterFader", volume);
+        RuntimeManager.StudioSystem.getParameterByName("MasterFader", out float value);
+        //Debug.Log("Valor actual del MasterFader: " + value);
     }
 }
