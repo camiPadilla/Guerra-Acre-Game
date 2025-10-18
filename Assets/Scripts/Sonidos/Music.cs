3f4a363ab3465b582ca353f57dd87d0016188040
el-Marce <gallardocoriamarcelodaniel@gmail.com>
2025-10-18 12:27:32 -0400

    Persistencia de ajustes de volumen entre escenas

--- Assets/Scripts/Sonidos/Music.cs
+++ Assets/Scripts/Sonidos/Music.cs
@@ -2,15 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using FMODUnity;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Music : MonoBehaviour
 {
-    [SerializeField] StudioEventEmitter musicaNivel1Emitter;
-    [SerializeField] StudioEventEmitter musicaNivel2Emitter;
+    [SerializeField] EventReference musicNivel_1;
+    [SerializeField] EventReference musicNivel_2;
 
     private void OnEnable()
     {
 
     }
+    void Start()
+    {
+
+        Scene escenaActiva = SceneManager.GetActiveScene();
+
+        if (escenaActiva.name == "EscenaUno")
+        {
+            RuntimeManager.PlayOneShot(musicNivel_1);
+        }
+        else if (escenaActiva.name == "EscenaDos")
+        {
+            RuntimeManager.PlayOneShot(musicNivel_2);
+        }
+        Debug.Log("La escena activa es: " + escenaActiva.name);
+    }
+
 
 }
