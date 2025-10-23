b369dc6a9fddc22ec66223b1de613e8107dbb3c4
ElTaqui <luis4lexander@gmail.com>
2025-10-22 21:25:02 -0400

    Jugabilidad cacho puntaje para el primerjugador

--- Assets/Scripts/JuegoDado/Dado.cs
+++ Assets/Scripts/JuegoDado/Dado.cs
@@ -4,25 +4,66 @@ using UnityEngine;
 
 public class Dado : MonoBehaviour
 {
-    [SerializeField] List<Sprite> caras = new List<Sprite>();
-    [SerializeField] SpriteRenderer miSprite;
-    [SerializeField] int cara;
+    [SerializeField] private List<Sprite> caras = new List<Sprite>();
+    [SerializeField] private SpriteRenderer miSprite;
+    [SerializeField] private int cara;
+    public bool usable;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        Girar();
+        //Girar();
+        miSprite = gameObject.GetComponent<SpriteRenderer>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-     public void Girar()
+    public void Girar()
     {
         int random = Random.Range(1, 6);
-        miSprite.sprite = caras[random-1];
-        print(random);
+        cara = random;
+        miSprite.sprite = caras[cara - 1];
+    }
+    public void Oponer()
+    {
+        switch (cara)
+        {
+            case 1: cara = 6; break;
+            case 2: cara = 5; break;
+            case 3: cara = 4; break;
+            case 4: cara = 3; break;
+            case 5: cara = 2; break;
+            case 6: cara = 1; break;
+        }
+        miSprite.sprite = caras[cara - 1];
+        usable = false;
+        JuegoDadoManager.Instance.bonusPrimera = 0;
+        JuegoDadoManager.Instance.ContarPuntos();
+        JuegoDadoManager.Instance.AumentarVuelta();
+    }
+    private void OnMouseDown()
+    {
+        if (usable && JuegoDadoManager.Instance.PuedeVoltear())
+        {
+            //Oponer();
+            animator.SetTrigger("Cambiar");
+            //Debug.Log(cara);
+
+        }
+    }
+    private void OnMouseOver()
+    {
+        if (usable)
+        {
+            //animacion para interactuar
+            animator.SetBool("Mouse", true);
+        }
+    }
+    private void OnMouseExit()
+    {
+        animator.SetBool("Mouse", false);
+    }
+    public int GetCara()
+    {
+        return cara;
     }
 }
--- Assets/Scripts/JuegoDado/JuegoDadoManager.cs
+++ Assets/Scripts/JuegoDado/JuegoDadoManager.cs
@@ -1,21 +1,263 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JuegoDadoManager : MonoBehaviour
 {
-    [SerializeField] List<GameObject> Dados;
-    int contadorClicks;
-    Queue<GameObject> dadosSeleccionados;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<Dado> dados;
+    [SerializeField] private VasoManager vaso;
+    private int escalera,full, poker, grande;
+    private int[] puntos = new int[6];
+    public int bonusPrimera;
+    [SerializeField]
+    private GameObject anote;
+    [SerializeField]
+    private GameObject[] UI;
+    [SerializeField]
+    private TextMeshProUGUI[] puntosUI;
+    [SerializeField]
+    private TextMeshProUGUI puntajeTotal;
+    [SerializeField] 
+    private int cantidadTirosMax;
+    private int puntaje, puntajeEnemigo;
+    private int vueltas;
+    private int cantidadTiros;
+    private bool botones;
+
+
+    public static JuegoDadoManager Instance { get; private set; }
+    void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        cantidadTiros = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DesactivarDados()
     {
-        
+        for (int i = 0; i < dados.Count; i++)
+        {
+            dados[i].usable = false;
+        }
     }
+    public void ActivarDados()
+    {
+        for (int i = 0; i < dados.Count; i++)
+        {
+            dados[i].usable = true;
+        }
+    }
+    public void DesactivarVaso()
+    {
+        vaso.usable = false;
+    }
+    public void ActivarVaso()
+    {
+        vaso.usable = true;
+    }
+    public void Tirar()
+    {
+        if (cantidadTiros <= cantidadTirosMax)
+        {
+            vueltas = 0;
+            for (int i = 0; i < dados.Count; i++)
+            {
+                dados[i].Girar();
+            }
+            bonusPrimera = 5;
+            ContarPuntos();
+            ActivarDados();
+            cantidadTiros++;
+            vaso.usable = false;
+            botones = true;
+        }
+    }
+    public void ContarPuntos()
+    {
+        puntos = new int[6];
+        escalera = full = poker = grande = 0;
+        int[] frecuencia = new int[7];
+        bool par = false;
+        bool trio = false;
+        bool cuatro = false; 
+        bool cinco = false;
+
+        for (int i = 0; i < dados.Count; i++)
+        {
+            int cara = dados[i].GetCara();
+            frecuencia[cara]++;
+
+            //switch (cara)
+            //{
+            //    case 1: balas = balas +1; break;
+            //    case 2: tontos = tontos + 2; break;
+            //    case 3: trenes = trenes + 3; break;
+            //    case 4: cuadras = cuadras + 4; break;
+            //    case 5: quinas = quinas + 5; break;
+            //    case 6: cenas = cenas + 6; break;
+            //}
+        }
+        for (int i = 1; i <= 6 ; i++)
+        {
+            puntos[i - 1] = i * frecuencia[i];
+            if (frecuencia[i] == 2)
+            {
+                par = true;
+            }else 
+            if (frecuencia[i] == 3)
+            {
+                trio = true;
+            }else
+            if (frecuencia[i] == 4)
+            {
+                cuatro = true;
+            }else
+            if (frecuencia[i] == 5)
+            {
+                cinco = true;
+            }
+        }
+
+        if ((frecuencia[2] == 1 && frecuencia[3] == 1 && frecuencia[4] == 1 && frecuencia[5] == 1 && frecuencia[6] == 1) ||
+        (frecuencia[1] == 1 && frecuencia[2] == 1 && frecuencia[3] == 1 && frecuencia[4] == 1 && frecuencia[5] == 1))
+        {
+            escalera = 20 + bonusPrimera;
+        }else if (par && trio)
+        {
+            full = 30 + bonusPrimera;
+        }else if (cuatro)
+        {
+            poker = 40 + bonusPrimera;
+        }else if (cinco)
+        {
+            if (bonusPrimera > 0)
+            {
+                Debug.Log("Ganaste por dormida");
+            }
+            else
+            {
+                grande = 50;
+            }
+        }
+        MostrarPuntos();
+    }
+    private void MostrarPuntos()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (i < 6)
+            {
+                puntosUI[i].text = "" + puntos[i];
+            }
+            else
+            {
+                switch (i)
+                {
+                    case 6:
+                        puntosUI[i].text = "" + escalera;
+                        break;
+                    case 7:
+                        puntosUI[i].text = "" + full;
+                        break;
+                    case 8:
+                        puntosUI[i].text = "" + poker;
+                        break;
+                    case 9:
+                        puntosUI[i].text = "" + grande;
+                        break;
+                }
+            }
+        }
+    }
+    public bool PuedeVoltear()
+    {
+        if (vueltas < 2)
+        {
+            return true;
+        }
+        else
+        {
+            DesactivarDados();
+            return false;
+        }
+    }
+    public void AumentarVuelta()
+    {
+        vueltas++;
+    }
+    public void Anotar(int i)
+    {
+        if ((cantidadTiros < cantidadTirosMax) && botones)
+        {
+            if (i < 6)
+            {
+                puntaje = puntaje + puntos[i];
+            }
+            else
+            {
+                switch (i)
+                {
+                    case 6:
+                        puntaje = puntaje + escalera;
+                        break;
+                    case 7:
+                        puntaje = puntaje + full;
+                        break;
+                    case 8:
+                        puntaje = puntaje + poker;
+                        break;
+                    case 9:
+                        puntaje = puntaje + grande;
+                        break;
+                }
+            }
+            botones = false;
+        }
+        puntajeTotal.text = puntaje.ToString();
+        //turno Oponente
+        vaso.usable = true;
+        if(cantidadTiros == cantidadTirosMax)
+        {
+            EvaluarVictoria();
+            vaso.usable = false;
+        }
+    }
+    public void Prueba(Button boton)
+    {
+        if (!botones)
+        {
+            boton.interactable = true;
+        }
+    }
+    public void EvaluarVictoria()
+    {
+        if (puntaje > puntajeEnemigo)
+        {
+            Ganar();
+        }
+        else
+        {
+            Perder();
+        }
+    }
+    public void Ganar()
+    {
+        Debug.Log("Ganaste");
+    }
+    public void Perder()
+    {
+        Debug.Log("Perdiste");
+
+    }
+
 }
