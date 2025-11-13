369b0e81c86708d46467591680cd167b2de829fa
ElTaqui <luis4lexander@gmail.com>
2025-11-13 09:57:19 -0400

    Correcion Sprites Fusil

--- Assets/Scripts/Personajes Scripts/SpriteManager.cs
+++ Assets/Scripts/Personajes Scripts/SpriteManager.cs
@@ -6,7 +6,7 @@ public class SpriteManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] Jugador1;
     [SerializeField] private GameObject[] Jugador2;
-    [SerializeField] private GameObject[] c_Rmano, a_Rmano,m_Machete, m_Carrada, m_Fusil, c_Machete, s_Machete,  caraHerido;
+    [SerializeField] private GameObject[] c_Rmano, fusil_Rmano,m_Machete, m_Carrada, m_Abierta, c_Machete, s_Machete,  caraHerido;
     [SerializeField] private GameObject armadura1, armadura2;
     [SerializeField] private int jugador;
 
@@ -20,28 +20,28 @@ public class SpriteManager : MonoBehaviour
         {
             case 0:
                 c_Rmano[jugador].SetActive(true);
-                a_Rmano[jugador].SetActive(false);
+                fusil_Rmano[jugador].SetActive(false);
                 m_Machete[jugador].SetActive(true);
                 m_Carrada[jugador].SetActive(false);
-                m_Fusil[jugador].SetActive(false);
+                m_Abierta[jugador].SetActive(false);
                 c_Machete[jugador].SetActive(false);
                 s_Machete[jugador].SetActive(true);
                 break;
             case 1:
                 c_Rmano[jugador].SetActive(true);
-                a_Rmano[jugador].SetActive(false);
+                fusil_Rmano[jugador].SetActive(false);
                 m_Machete[jugador].SetActive(false);
                 m_Carrada[jugador].SetActive(true);
-                m_Fusil[jugador].SetActive(false);
+                m_Abierta[jugador].SetActive(false);
                 c_Machete[jugador].SetActive(true);
                 s_Machete[jugador].SetActive(false); 
                 break;
             case 2:
                 c_Rmano[jugador].SetActive(false);
-                a_Rmano[jugador].SetActive(true);
+                fusil_Rmano[jugador].SetActive(true);
                 m_Machete[jugador].SetActive(false);
                 m_Carrada[jugador].SetActive(false);
-                m_Fusil[jugador].SetActive(true);
+                m_Abierta[jugador].SetActive(true);
                 c_Machete[jugador].SetActive(true);
                 s_Machete[jugador].SetActive(false);
                 break;
