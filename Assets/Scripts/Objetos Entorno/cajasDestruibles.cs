ecb0a0d40b46a3fe794f073eb8ed810301d44bf8
ElTaqui <144634063+ElTaqui@users.noreply.github.com>
2025-11-28 10:13:29 -0400

    Animación cajas y capas de prefabs

--- Assets/Scripts/Objetos Entorno/cajasDestruibles.cs
+++ Assets/Scripts/Objetos Entorno/cajasDestruibles.cs
@@ -6,10 +6,15 @@ public class cajasDestruibles : ObjetoDestruible
 {
 
     [SerializeField] bool conLoot;
-    // Start is called before the first frame update
-    
+    private Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void ActivarLoot()
     {
-        if(conLoot)   ObjetosManager.instancia.InstanciarObjeto(transform.position);
+        animator.SetTrigger("destruir");
+        if (conLoot)   ObjetosManager.instancia.InstanciarObjeto(transform.position);
     }
 }
